dfa6d48c8212325ca977032ee2753fa380e5cc79
Apathy <frolinfn@gmail.com>
2024-08-09 15:52:47 +0300

    Add project files.

---
+++ WinRar bruteforcer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "Zipwn | Made by Ebola Man";
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("\n" +
+            "   ███████╗██╗██████╗ ██╗    ██╗███╗   ██╗\n" +
+            "   ╚══███╔╝██║██╔══██╗██║    ██║████╗  ██║\n" +
+            "     ███╔╝ ██║██████╔╝██║ █╗ ██║██╔██╗ ██║\n" +
+            "    ███╔╝  ██║██╔═══╝ ██║███╗██║██║╚██╗██║\n" +
+            "   ███████╗██║██║     ╚███╔███╔╝██║ ╚████║\n" +
+            "   ╚══════╝╚═╝╚═╝      ╚══╝╚══╝ ╚═╝  ╚═══╝");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("" +
+        "             Made By Ebola Man         \n");
+        if (!Directory.Exists(@"C:\Program Files\7-Zip"))
+        {
+            Console.WriteLine("7-Zip not installed!");
+            wait();
+        }
+        Console.Write("Enter Archive(path): ");
+        string archive = Console.ReadLine();
+        if (!File.Exists(archive))
+        {
+            Console.WriteLine("Archive not found!");
+            wait();
+        }
+        Console.Write("Enter Wordlist(path): ");
+        string wordlist = Console.ReadLine();
+        if (!File.Exists(wordlist))
+        {
+            Console.WriteLine("Wordlist not found!");
+            wait();
+        }
+        Console.WriteLine("Cracking...");
+        foreach (string password in File.ReadLines(wordlist))
+        {
+            Console.WriteLine($"ATTEMPT : {password}");
+            if (Attempt(archive, password))
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"\nSuccess! Password Found: {password}");
+                wait();
+            }
+        }
+
+        Console.WriteLine("shitty wordlist dumbass");
+        wait();
+    }
+    static bool Attempt(string archive, string password)
+    {
+        var processInfo = new ProcessStartInfo
+        {
+            FileName = @"C:\Program Files\7-Zip\7z.exe",
+            Arguments = $"x -p{password} \"{archive}\" -o\"cracked\" -y",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        using (var process = Process.Start(processInfo))
+        {
+            process.WaitForExit();
+            return process.ExitCode == 0;
+        }
+    }
+    static void wait()
+    {
+        Console.WriteLine("Press any key to continue...\n#FREE EbolaMan");
+        Console.ReadKey();
+        Environment.Exit(0);
+    }
+}
---
+++ WinRar bruteforcer/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("WinRar Bruteforcer")]
+[assembly: AssemblyDescription("WinRar Bruteforcer by Ebola Man")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("#FREE Ebola Man")]
+[assembly: AssemblyProduct("WinRar Bruteforcer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5447ee86-df25-4ec2-a0ef-039af80f64f4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
